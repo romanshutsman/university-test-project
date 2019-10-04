6fdb71c48b634790dd72e53f41f77bf9aa75a17f
roman.shutsman@Blackthorn-vision.com <roman.shutsman@Blackthorn-vision.com>
2019-10-04 19:58:16 +0300

    create project, connect Firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SignIn  from './SignIn'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    return (
+      <div className="App">
+        <SignIn />
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+
+          {
+            user
+              ? <Button variant="contained" color="primary" onClick={signOut}>Sign out</Button>
+              : <Button variant="contained" color="primary" onClick={signInWithGoogle}>Sign in with Google</Button>
+          }
+        </header> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDHVFD2LI4FGcjO3j8QZcqtU7TGzRgJmpI",
+    authDomain: "university-test-project-e4715.firebaseapp.com",
+    databaseURL: "https://university-test-project-e4715.firebaseio.com",
+    projectId: "university-test-project-e4715",
+    storageBucket: "university-test-project-e4715.appspot.com",
+    messagingSenderId: "899544141806",
+    appId: "1:899544141806:web:e4c7687b90dafef18e08ff",
+    measurementId: "G-P97HQXTLGL"
+};
+export default config;
